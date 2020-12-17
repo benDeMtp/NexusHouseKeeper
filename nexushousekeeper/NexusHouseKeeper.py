5bdcc8e779eeb9bbffc5e17bba8db0059ee4191b
Benjamin <psychoben@gmail.Com>
2020-12-17 16:57:02 +0100

    Add progress bar

--- nexushousekeeper/NexusHouseKeeper.py
+++ nexushousekeeper/NexusHouseKeeper.py
@@ -5,9 +5,12 @@ import re
 import datetime
 from rich.console import Console
 from rich.table import Table
+from rich.progress import Progress
 from hurry.filesize import size
 from requests_futures.sessions import FuturesSession
 from concurrent.futures import as_completed
+import asyncio
+import math
 
 
 class NexusHouseKeeper:
@@ -20,14 +23,14 @@ class NexusHouseKeeper:
     date_pattern = "(\d{8}\.\d{6})"
     snapshot_finder = re.compile(version_pattern + "-?" + date_pattern + "?-?\d*")
 
-
-
-    def __init__(self, user, password, nexus_url, repository, dryRun=False):
+    def __init__(self, user, password, nexus_url, repository, dry_run=False, parallelism=20):
         self.cred = HTTPBasicAuth(user, password)
         self.nexus_url = nexus_url
         self.repository = repository
-        self.dryRun = dryRun
+        self.dryRun = dry_run
         self.versions_cache = {}
+        self.parallelism = parallelism
+        self.console = Console()
 
     def _get_all_components(self, token=None):
         params = {'repository': self.repository}
@@ -56,38 +59,44 @@ class NexusHouseKeeper:
     def _get_components_as_list(self, fun, **args) -> list:
         response = fun(**args)
         components = self._fill_tmp_array_from_json(response.json())
-        while 'continuationToken' in response.json() and response.json()['continuationToken']:
-            response = fun(token=response.json()['continuationToken'], **args)
-            components += (self._fill_tmp_array_from_json(response.json()))
+        with self.console.status("[bold green]Gathering components metadata...") as status:
+            while 'continuationToken' in response.json() and response.json()['continuationToken']:
+                response = fun(token=response.json()['continuationToken'], **args)
+                components += (self._fill_tmp_array_from_json(response.json()))
         return components
 
-    def show_all_components(self) -> None:
+    def show_all_components(self, with_size=True) -> None:
         components = self._get_components_as_list(self._get_all_components)
-        aggregates_components,total_size = self.aggregates_components(components)
-        console = Console()
+        aggregates_components, total_size = self.aggregates_components(components, with_size)
+
         table = Table(show_header=True, header_style="bold magenta")
         table.add_column("Name", style="dim")
         table.add_column("Versions")
         for k, v in aggregates_components.items():
             table.add_row(k, ", ".join(v))
-        console.print(table)
-        console.print("Total size : " + size(total_size))
+        self.console.print(table)
+        self.console.print("Total size : " + size(total_size))
 
-    def aggregates_components(self, components :list) -> tuple:
+    def aggregates_components(self, components: list, with_size: bool) -> tuple:
         aggregates = {}
+        total_size = 0
 
-        def _add_to_aggregates(key,version,comp_size):
+        def _add_to_aggregates(key, version, comp_size=None):
+            size_as_string = ""
+            if comp_size:
+                size_as_string = " [" + size(comp_size) + "]"
             if key in aggregates:
-                aggregates[key].add(version + " [" + size(comp_size) + "]")
+                aggregates[key].add(version + size_as_string)
             else:
                 aggregates[key] = set()
-                aggregates[key].add(version + " [" + size(comp_size) + "]")
+                aggregates[key].add(version + size_as_string)
 
-        regex = re.compile("^(.*)-" + self.date_pattern + "-?\d*")
-        total_size = 0
-        for x in components:
-            comp_size = self._components_size(x)
-            total_size += comp_size
+        async def _handle_components(x, with_size):
+            comp_size = None
+            if with_size:
+                comp_size = await loop.run_in_executor(None, self._components_size, x)
+                nonlocal total_size
+                total_size += comp_size
             key = x["group"] + ":" + x["name"]
             match = regex.match(x["version"])
             if match:
@@ -95,14 +104,28 @@ class NexusHouseKeeper:
                 version = match.group(1)
                 if match.group(2):
                     version += "-SNAPSHOT"
-                _add_to_aggregates(key,version,comp_size)
+                _add_to_aggregates(key, version, comp_size)
             else:
                 if re.compile("^[0-9\.]*").match(x["version"]):
-                    _add_to_aggregates(key,x["version"],comp_size)
+                    _add_to_aggregates(key, x["version"], comp_size)
                 else:
-                    print(x["version"]+" ignored")
+                    print(x["version"] + " ignored")
 
-        return aggregates,total_size
+        regex = re.compile("^(.*)-" + self.date_pattern + "-?\d*")
+
+        with Progress() as progress:
+            ceil = math.ceil(len(components) / self.parallelism)
+            gather_data = progress.add_task("[green]Gathering size data ....", total=ceil)
+
+            for batch in self.batch_generator(components, self.parallelism):
+                tasks = [_handle_components(x, with_size) for x in batch]
+                # for x in components:
+                #    _handle_components(x)
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(asyncio.gather(*tasks))
+                progress.update(gather_data, advance=1)
+
+        return aggregates, total_size
 
     def _fill_tmp_array_from_json(self, json) -> list:
         components = []
@@ -155,15 +178,30 @@ class NexusHouseKeeper:
         :return: None
         """
         total_size = 0
-        console = Console()
-        for comp in components:
-            total_size += self._components_size(comp)
+
+        async def _handle_components(comp):
+            comp_size = self._components_size(comp)
+            nonlocal total_size
+            total_size += comp_size
             if self.dryRun:
-                console.print(
-                    "deleting " + comp['name'] + ':' + comp['version'] + ' ' + size(self._components_size(comp)))
+                self.console.print(
+                    "deleting " + comp['name'] + ':' + comp['version'] + ' ' + size(comp_size))
             else:
                 self._delete_component(comp['id'])
-        console.print("Free memory :[bold]" + size(total_size) + "[/bold]")
+
+        with Progress() as progress:
+            ceil = math.ceil(len(components) / self.parallelism)
+            gather_data = progress.add_task("[green]Deleting components ....", total=ceil, visible=not self.dryRun)
+
+            for batch in self.batch_generator(components, self.parallelism):
+                tasks = [_handle_components(x) for x in batch]
+                # for x in components:
+                #    _handle_components(x)
+                loop = asyncio.get_event_loop()
+                loop.run_until_complete(asyncio.gather(*tasks))
+                progress.update(gather_data, advance=1)
+
+        self.console.print("Free memory :[bold]" + size(total_size) + "[/bold]")
 
     def delete_all_components_by_version(self, version):
         self._delete_components_in_array(self._get_components_as_list(self._search_components, version=version))
@@ -239,31 +277,41 @@ class NexusHouseKeeper:
             print("ignore version " + component["version"] + " pour l'artefact " + component["group"] + ":" + component[
                 "name"])
 
+    def batch_generator(self, lst, n):
+        """Yield successive n-sized chunks from lst."""
+        for i in range(0, len(lst), n):
+            yield lst[i:i + n]
+
 
 def main():
     parser = argparse.ArgumentParser(description="Script permetant de faire des opérations sur des composants nexus")
-    parser.add_argument("-u", help="nom de l'utilisateur nexus",required=True)
-    parser.add_argument("-p", help="mot de passe de l'utilisateur nexus",required=True)
-    parser.add_argument("-r", help="repository",required=True)
+    parser.add_argument("-u", help="nom de l'utilisateur nexus", required=True)
+    parser.add_argument("-p", help="mot de passe de l'utilisateur nexus", required=True)
+    parser.add_argument("-r", help="repository", required=True)
     parser.add_argument("-s", help="affiche l'ensemble des versions pour chaque composants", action="store_true")
-    parser.add_argument("--nexus-url", help="la base path de l'api nexus",required=True)
+    parser.add_argument("--nexus-url", help="la base path de l'api nexus", required=True)
     parser.add_argument("--version-match",
                         help="supprime tous les artefacts dont le numéro de version réponds à l'expression")
     parser.add_argument("--version",
                         help="delete all components with this exact version")
     parser.add_argument("-l",
-                        help="conserve uniquement les n dernière version. Ne fonctionne uniquement qu'avec les versions au format X.Y.Z")
+                        help="conserve uniquement les n dernière version. Ne fonctionne uniquement qu'avec les "
+                             "versions au format X.Y.Z")
     parser.add_argument("--dryrun",
                         help="n'execute pas réellement la requête mais affiche les composants potentiellement effacés",
                         action="store_true")
+    parser.add_argument("--parallel",
+                        help="number of parallel tasks (default 20)", default=20)
+    parser.add_argument("--no-size",
+                        help="don't grab size of each object", action="store_false", default=True)
     # TODO rendre des paramètre obligatoire
     args = parser.parse_args()
 
-    nexus = NexusHouseKeeper(args.u, args.p, args.nexus_url, args.r, args.dryrun)
+    nexus = NexusHouseKeeper(args.u, args.p, args.nexus_url, args.r, args.dryrun, int(args.parallel))
     if args.version_match:
         nexus.delete_all_component_by_version_pattern(version_pattern=args.version_match)
     elif args.s:
-        nexus.show_all_components()
+        nexus.show_all_components(args.no_size)
     elif args.l:
         nexus.keep_lasts_versions(args.l)
     elif args.version:
